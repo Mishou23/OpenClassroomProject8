7debf1006a7f1c48ef63b936fd8853c7b14df68c
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-09-09 10:38:40 -0400

    Header added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,32 @@
 import './index.css'
+import { Link } from 'react-router-dom';
+import imgSrc from '../../../images/IMG.png';
+import logoSrc from '../../../images/LOGO.png';
 export default function Header() {
     return (
-  <div>
-    <h1>Hello World</h1>
-  </div>
+      <div>
+      <div className="navbar">
+        <div className="logo">
+          <img src={logoSrc} alt="Logo" className="logo-img" />
+        </div>
+        <ul className="navbar-ul">
+          <li>
+            <Link to="/" className="nav-link">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav-link">A Propos</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="header">
+        <div className="imageContainer">
+          <h1 className="header-text">Chez vous, partout et ailleurs</h1>
+          <div className="imageOverlay"></div>
+          <img src={imgSrc} alt="Image Description" className="header-img" />
+        </div>
+      </div>
+    </div>
+
     )
   }
 
