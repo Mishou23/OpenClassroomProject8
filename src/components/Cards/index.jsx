0d7b647759c4584f8e258e8bf72b21fef70acd3d
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-09-09 11:19:13 -0400

    Appartements data rendered to Card component

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,8 +1,14 @@
 import "./index.css"
+import Data from '../../logements'
 export default function Cards() {
     return (
-  <div>
-    
-  </div>
+      <div className="card-container">
+      {Data.map((item, index) => (
+        <div key={index} className="card">
+          <img src={item.cover} alt={item.title} />
+          <h2>{item.title}</h2>
+        </div>
+      ))}
+    </div>
     )
   }
