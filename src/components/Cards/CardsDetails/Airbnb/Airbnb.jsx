14a24ce1f2e2a265030ff5140c63c41e5457f353
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-09-20 05:03:07 +0200

    Creating Host Ratings Stars

--- src/components/Cards/CardsDetails/Airbnb/Airbnb.jsx
+++ src/components/Cards/CardsDetails/Airbnb/Airbnb.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import Data from '../../../../logements';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
+
+// Define the StarRating component once
+const StarRating = ({ rating }) => {
+  const maxRating = 5; // Maximum rating possible
+
+  // Create an array of star elements
+  const stars = [];
+
+  // Add filled stars
+  for (let i = 0; i < maxRating; i++) {
+    const starIcon = i < rating ? solidStar : regularStar;
+    stars.push(
+      <span key={i} className="star">
+        <FontAwesomeIcon icon={starIcon} />
+      </span>
+    );
+  }
+
+  return <div className="star-rating">{stars}</div>;
+};
 
 export default function Airbnb() {
   const { id } = useParams(); // Get the ID from the URL
@@ -36,6 +59,10 @@ export default function Airbnb() {
                 <img src={selectedData.host.picture} alt={selectedData.host.name} />
               </div>
             </div>
+            <div className='ratings'>
+                <StarRating rating={selectedData.host.rating} />
+                {console.log(selectedData.rating)}
+              </div>
           </div>
         </div>
       </div>
